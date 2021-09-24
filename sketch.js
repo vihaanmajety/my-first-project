0e2f1268ee518655666b2173ff8e4803d1100467
vihaanmajety <91346819+vihaanmajety@users.noreply.github.com>
2021-09-24 16:51:29 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(800,800);
+  var sprite=createSprite(695,574,8,24)
+  sprite.shapeColor="green"
+}
+
+function draw() 
+{
+  background("blue");
+drawSprites()
+}
+
+
+
+
